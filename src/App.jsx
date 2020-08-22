e75fc5dde73998b1cf842bb4e3758f393d487b19
Tom <tom.vinnicombe1@gmail.com>
2020-08-22 20:39:14 +1000

    Add sorting

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.scss';
 const MOCK_DATA = {"dashorde": [{"player": "Dashorde", "games": "2", "wins": 1, "losses": 1, "winrate": "50.00"}, {"player": "urqt314gf", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "Lets Go Gank Bot", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "grandb369", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "rambo779", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "H1FakerBaker", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, {"player": "RezWez", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, {"player": "boops2", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, {"player": "lethal03", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, {"player": "1M0NSTER1", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, 
 {"player": "B1G D1CK DRAVEN", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "MasterMelhem", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "Devinu", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "dfadf", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "Toltorn", "games": "1", "wins": 0, "losses": 1, "winrate": "0.00"}, {"player": "Kushina ", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, {"player": "Andrew135", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, {"player": "natsuba", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}, {"player": "XiaoMing2018", "games": "1", "wins": 1, "losses": 0, "winrate": "100.00"}]};
 
-const getPlayerRecords = (playerName) => axios.get(`http://ec2-18-221-150-128.us-east-2.compute.amazonaws.com/winrates/${playerName}`, {
+const getPlayerRecords = (playerName) => axios.get(`http://ec2-3-22-20-210.us-east-2.compute.amazonaws.com/winrates/${playerName}`, {
 	headers: {
 	  'Access-Control-Allow-Origin': '*',
   }
@@ -18,12 +18,39 @@ const mockGetPlayerRecords = (playerName) => new Promise((resolve, reject) => {
   }, 300);
 });
 
+const COLUMNS = [
+    {
+      Header: 'Summoner',
+      accessor: 'player',
+    },
+    {
+      Header: 'Games Played',
+      accessor: 'games',
+      isSorted: true
+    },
+    {
+      Header: 'Wins',
+      accessor: 'wins',
+    },
+    {
+      Header: 'Losses',
+      accessor: 'losses',
+    },
+    {
+      Header: 'Win Rate',
+      accessor: 'winrate',
+    }];
+
+// const data = React.useMemo(() => makeData(2000), [])
+
 function App() {
   const [inputValue, setInputValue] = useState('dashorde');
   const [player, setPlayer] = useState('dashorde');
   const [gameRecords, setGameRecords] = useState([]);
   const [error, setError] = useState();
   const [searching, setSearching] = useState(true);
+  const columns = React.useMemo(() => COLUMNS, []);
+  const data = React.useMemo(() => gameRecords, [gameRecords])
 
   useEffect(() => {
     getPlayerRecords(player).then(({data}) => {
@@ -66,7 +93,7 @@ function App() {
       <header className="App-header">
         <h1>Project 0</h1>
         <form onSubmit={handleSubmit} className="form">
-          <input value={inputValue} className="form__input" type="text" onChange={handleInputChange} />
+          <input autoFocus={true} value={inputValue} className="form__input" type="text" onChange={handleInputChange} />
           <button className="form__button" type="submit">Check Player</button>
         </form>
         {
@@ -76,7 +103,7 @@ function App() {
           searching && <p>Searching...</p>
         }
         {
-          !error && !searching && <PlayedWithTable players={gameRecords} />
+          !error && !searching && <PlayedWithTable columns={columns} data={data} />
         }
       </header>
     </div>
--- src/components/PlayedWithTable/PlayedWithTable.jsx
+++ src/components/PlayedWithTable/PlayedWithTable.jsx
@@ -1,31 +1,81 @@
+import { useTable, useSortBy } from 'react-table'
 import React from 'react';
 import './PlayedWithTable.scss';
 
-const PlayedWithTable = ({ players }) => {
+const getCellClass = ({ column }) => {
+    switch (column.id) {
+        case 'wins':
+        case 'losses':
+        case 'games':
+        case 'winrate':
+            return 'align-right';
+        default:
+            return '';
+    };
+}
 
+const getCellSuffix = ({ column }) => {
+    switch (column.id) {
+        case 'winrate':
+            return '%';
+        default:
+            return '';
+    }
+}
+
+const PlayedWithTable = ({ columns, data }) => {
+    const {
+        getTableProps,
+        headerGroups,
+        getTableBodyProps,
+        rows,
+        prepareRow,
+      } = useTable(
+        {
+          columns,
+          data,
+        },
+        useSortBy
+      );
     return (
-        <table className="table">
+        <table className="table" {...getTableProps()}>
             <thead>
-                <tr>
-                    <th>Summoner</th>
-                    <th>Games Played</th>
-                    <th>Wins</th>
-                    <th>Losses</th>
-                    <th>Win Rate</th>
-                </tr>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                        // Add the sorting props to control sorting. For this example
+                        // we can add them into the header props
+                        <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                        {column.render('Header')}
+                        {/* Add a sort direction indicator */}
+                        <span className="sort-span">
+                            {column.isSorted
+                            ? column.isSortedDesc
+                                ? ' 🔽'
+                                : ' 🔼'
+                            : ''}
+                        </span>
+                        </th>
+                    ))}
+                    </tr>
+                ))}
             </thead>
-            <tbody>
-                {
-                    players.map(({ player, games, wins, losses, winrate }) => (
-                        <tr key={player}>
-                            <td>{player}</td>
-                            <td className="align-right">{games}</td>
-                            <td className="align-right">{wins}</td>
-                            <td className="align-right">{losses}</td>
-                            <td className="align-right">{winrate}%</td>
-                        </tr>
-                    ))
-                }
+            <tbody {...getTableBodyProps()}>
+                {rows.map(
+                    (row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    
+                                    return (
+                                    <td className={getCellClass(cell)} {...cell.getCellProps()}>{cell.render('Cell')}{getCellSuffix(cell)}</td>
+                                    )
+                                })}
+                            </tr>
+                        );
+                    }
+                )}
             </tbody>
         </table>
     );
